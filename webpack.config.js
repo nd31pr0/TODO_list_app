d7ddcf221ffcf56198697d091215f21be7ad5deb
Ndzi Nfor <ndzinfor@gmail.com>
2024-02-10 19:19:28 +0100

    Add config info to webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Entry point of the application
+  entry: './src/js/index.js',
+
+  // Output configuration
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Clean the output directory before emit
+  },
+
+  // Loaders and rules
+  module: {
+    rules: [
+      // CSS: Use style-loader and css-loader to process CSS files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  // Plugins
+  plugins: [
+    // HtmlWebpackPlugin to generate the output HTML file from the template
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  // Development server configuration (optional, if you decide to use webpack-dev-server)
+  devServer: {
+    static: './dist',
+    open: true,
+  },
+
+  // Mode
+  // Set to 'development' or 'production' depending on your needs
+  mode: 'development',
+};
