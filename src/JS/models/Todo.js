959c7ef03a68e22bf4cf3d9db1ad1b5813f47874
Ndzi Nfor <ndzinfor@gmail.com>
2024-02-12 08:10:36 +0100

    Add isComplete property to Todo class

--- src/JS/models/Todo.js
+++ src/JS/models/Todo.js
@@ -1,11 +1,12 @@
 class Todo {
-    constructor(title, description, dueDate, priority, notes, checklist) {
+    constructor(title, description, dueDate, priority, notes, checklist, isComplete) {
       this.title = title;
       this.description = description;
       this.dueDate = dueDate;
       this.priority = priority;
       this.notes = notes || '';
       this.checklist = checklist || []; // assuming checklist is an array of items
+      this.isComplete = isComplete;
     }
   }
   
